8f01c342ed3e87f175d3e0e504865d37ab1d5706
Sabrin <sabrinlalsingh@gmail.com>
2017-07-09 16:52:01 +0545

    First Servlet Program added

---
+++ Servlet_Example1/src/servlet/tutorial/HelloWorld.java
@@ -0,0 +1,40 @@
+package servlet.tutorial;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloWorld
+ */
+@WebServlet(description = "First Servlet HELLO WORLD Program", urlPatterns = { "/HelloWorld" })
+public class HelloWorld extends HttpServlet {
+	
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HelloWorld() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out=response.getWriter();
+		out.println("<h1>Hello World</h1>"
+				+ "<style>h1{color:red;}</style>");
+		String uname=request.getParameter("userName");
+		out.println("Hello " + uname);
+		
+	}
+
+	
+
+}
